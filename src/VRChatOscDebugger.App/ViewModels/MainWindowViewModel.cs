28c5559843141d8ca340c34cf5a6e4ef2700f590
Misaka-L <lipww1234@foxmail.com>
2024-07-12 22:16:04 +0800

    chore: add vrc-oscquery-lib submodule

--- src/VRChatOscDebugger.App/ViewModels/MainWindowViewModel.cs
+++ src/VRChatOscDebugger.App/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
-using VRC.OSCQuery;
-
-namespace VRChatOscDebugger.App.ViewModels;
+﻿namespace VRChatOscDebugger.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
